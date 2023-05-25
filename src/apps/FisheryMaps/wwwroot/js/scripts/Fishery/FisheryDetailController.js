bd509b4d998560bffa4af5bdfc66a3d58ed6d9d5
rflagg <richard.lee.flagg@outlook.com>
2023-05-25 19:40:52 -0400

    Fleshing out Fishery Detail

---
+++ src/apps/FisheryMaps/wwwroot/js/scripts/Fishery/FisheryDetailController.js
@@ -0,0 +1,22 @@
+/******/ (() => { // webpackBootstrap
+/******/ 	"use strict";
+var __webpack_exports__ = {};
+/*!****************************************************!*\
+  !*** ./scripts/Fishery/FisheryDetailController.ts ***!
+  \****************************************************/
+
+var FisheryDetailController = /** @class */ (function () {
+    function FisheryDetailController() {
+        this.WireEvents();
+    }
+    FisheryDetailController.prototype.WireEvents = function () {
+    };
+    return FisheryDetailController;
+}());
+window.onload = function () {
+    new FisheryDetailController();
+};
+
+/******/ })()
+;
+//# sourceMappingURL=FisheryDetailController.js.map
